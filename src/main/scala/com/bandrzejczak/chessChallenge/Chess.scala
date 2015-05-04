8c08d4ee0904e5e282d8b2d29bb7800031b5e566
Bartek Andrzejczak <ba.andrzejczak@gmail.com>
2015-05-04 22:42:38 +0200

    Adds test for 3x3 chessboard

--- src/main/scala/com/bandrzejczak/chessChallenge/Chess.scala
+++ src/main/scala/com/bandrzejczak/chessChallenge/Chess.scala
@@ -6,16 +6,16 @@ object Chess {
   def place(figuresToPlace: List[FigureType], size: Size) : List[Figure] =
     placeFigures(figuresToPlace, Square.generateChessboard(size.width, size.height), List[Figure]())
 
-  def placeFigures(figuresToPlace: List[FigureType], emptySquares: Seq[Square], placedFigures: List[Figure]): List[Figure] = {
+  def placeFigures(figuresToPlace: List[FigureType], availableSquares: Seq[Square], placedFigures: List[Figure]): List[Figure] = {
     figuresToPlace match {
       case Nil => placedFigures
       case f :: fs =>
-        if(emptySquares.isEmpty) throw new IllegalStateException
-        val figure = Figure.fromType(f, emptySquares.head)
+        if(availableSquares.isEmpty) throw new IllegalStateException
+        val figure = Figure.fromType(f, availableSquares.head)
         if(figure doesntBeatAny placedFigures)
-          placeFigures(fs, emptySquares.tail filter figure.doesntBeatOn, figure :: placedFigures)
+          placeFigures(fs, availableSquares.tail filter figure.doesntBeatOn, figure :: placedFigures)
         else
-          placeFigures(figuresToPlace, emptySquares.tail, placedFigures)
+          placeFigures(figuresToPlace, availableSquares.tail, placedFigures)
     }
   }
 }
--- src/test/scala/com/bandrzejczak/chessChallenge/ChessSpec.scala
+++ src/test/scala/com/bandrzejczak/chessChallenge/ChessSpec.scala
@@ -32,6 +32,13 @@ class ChessSpec extends WordSpec with Matchers {
       //then
       figures should contain only (Rook(1,1), Rook(2,2))
     }
+
+    "place two kings and a rook on 3x3 chessboard" in {
+      //when
+      val figures = Chess.place(List(FigureType.King, FigureType.King, FigureType.Rook), 3 x 3)
+      //then
+      figures should contain only (King(1,1), King(1,3), Rook(3,2))
+    }
   }
 
 }
