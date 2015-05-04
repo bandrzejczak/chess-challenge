60364ad4582bc3a0a8a6f4bec15e53cbd5704eab
Bartek Andrzejczak <ba.andrzejczak@gmail.com>
2015-05-04 19:52:38 +0200

    Adds Queen figure

--- src/test/scala/com/bandrzejczak/chessChallenge/PiecesSpec.scala
+++ src/test/scala/com/bandrzejczak/chessChallenge/PiecesSpec.scala
@@ -11,8 +11,33 @@ class PiecesSpec extends WordSpec with Matchers {
     }
 
     "not beat on farther squares" in {
-      val king = King(3,7)
-      assert(king doesntBeatOn Square(4,5))
+      val king = King(3, 7)
+      assert(king doesntBeatOn Square(4, 5))
+    }
+  }
+
+  "A queen" should {
+    "beat in x axis" in {
+      val queen = Queen(4, 4)
+      for(x <- 1 to 8)
+        assert(queen beatsOn Square(x, 4))
+    }
+
+    "beat in y axis" in {
+      val queen = Queen(4, 4)
+      for(y <- 1 to 8)
+        assert(queen beatsOn Square(4, y))
+    }
+
+    "beat across" in {
+      val queen = Queen(4, 6)
+      for ((x, y) <- (1 to 6) zip (3 to 8))
+        assert(queen beatsOn Square(x,y))
+    }
+
+    "not beat in other squares" in {
+      val queen = Queen(6, 4)
+      assert(queen doesntBeatOn Square(3, 3))
     }
   }
 
@@ -29,4 +54,9 @@ sealed trait Figure {
 case class King(x: Int, y: Int) extends Figure {
   override def beatsOn(square: Square) : Boolean =
     Math.abs(x - square.x) <= 1 && Math.abs(y - square.y) <= 1
+}
+
+case class Queen(x: Int, y: Int) extends Figure {
+  override def beatsOn(square: Square): Boolean =
+    y == square.y || x == square.x || (y-square.y == x-square.x)
 }
