966ebe0915dc822e14c2fb64bd08f39acfaac3f0
Bartek Andrzejczak <ba.andrzejczak@gmail.com>
2015-05-08 22:17:42 +0200

    Add explicit return types of methods in implicit classes

--- src/main/scala/com/bandrzejczak/chessChallenge/implicits/implicits.scala
+++ src/main/scala/com/bandrzejczak/chessChallenge/implicits/implicits.scala
@@ -4,16 +4,17 @@ import com.bandrzejczak.chessChallenge.FigureType.FigureType
 
 package object implicits {
   implicit class FiguresList(list: List[Figure]) {
-    def withFigureTypeSameAs(figure: Figure) = withFigureTypeEqual(figure.figureType)
-    def withFigureTypeEqual(figureType: FigureType) = list filter (a => figureType == a.figureType)
-    def hasAllElementsContainedIn(otherList: List[Figure]) = list forall (otherList.contains(_))
+    def withFigureTypeSameAs(figure: Figure): List[Figure] = withFigureTypeEqual(figure.figureType)
+    def withFigureTypeEqual(figureType: FigureType): List[Figure] = list filter (a => figureType == a.figureType)
+    def hasAllElementsContainedIn(otherList: List[Figure]): Boolean = list forall (otherList.contains(_))
   }
 
   implicit class SolutionsSet(solutions: Set[List[Figure]]) {
-    def withFigureTypeSameAs(figure: Figure) = solutions map (solution => solution withFigureTypeSameAs figure)
+    def withFigureTypeSameAs(figure: Figure): Set[List[Figure]] =
+      solutions map (solution => solution withFigureTypeSameAs figure)
   }
 
   implicit class SquaresSeq(seq: Seq[Square]) {
-    def leftAfterPlacing(figure: Figure) = seq diff List(figure.thisSquare) filter figure.doesntBeatOn
+    def leftAfterPlacing(figure: Figure): Seq[Square] = seq diff List(figure.thisSquare) filter figure.doesntBeatOn
   }
 }
